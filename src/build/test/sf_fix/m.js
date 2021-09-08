f53dc728fa7afbe2cd5939be2a59c09ecd409334
ZeroDeaths <84610067+LearningBotCoding@users.noreply.github.com>
2021-09-08 16:58:55 +0530

    Create m.js

---
+++ src/build/test/sf_fix/m.js
@@ -0,0 +1,34 @@
+
+selector:
+  js_test:
+    buildscripts/tests/selftest_fixtures/one.js:
+    - tag1
+    - tag2
+    - tag3
+    buildscripts/tests/selftest_fixtures/two.js:
+    - tag1
+    - tag2
+
+selector:
+  js_test:
+    buildscripts/tests/selftest_fixtures/two.js:
+    - tag4
+
+selector:
+  js_test:
+    buildscripts/tests/selftest_fixtures/one.js:
+    - tag1
+    - tag2
+    - tag3
+    buildscripts/tests/selftest_fixtures/two.js:
+    - tag1
+    - tag2
+
+selector:
+  js_test:
+    buildscripts/tests/selftest_fixtures/two.js:
+    - tag4
+
+
+
+
